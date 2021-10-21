348c13c67c1af2d06002f82a30997e198cb869f3
Thomas Raines <thomasmraines@gmail.com>
2021-10-21 19:32:59 -0400

    Update Matt How to collect user input.java

--- Matt How to collect user input.java
+++ Matt How to collect user input.java
@@ -3,9 +3,16 @@ import java.util.Scanner;  // Import the Scanner class
 class Main {
   public static void main(String[] args) {
     Scanner myObj = new Scanner(System.in);  // Create a Scanner object
-    System.out.println("Enter username");
 
-    String timeDateString = myObj.nextLine();  // Read user input
-    System.out.println("Time Date is: " + timeDateString);  // Output user input
+    System.out.println("Enter A Time: "); // Prompt user to enter time
+
+    String timeString = myObj.nextLine();  // Read user input
+
+    System.out.println("Enter a Date: "); // prompt user to enter date
+
+    String dateString = myObj.nextLine(); // store that input into date string (read the user input) 
+
+
+    System.out.println("Time is: " + timeString + " and The date is: " + dateString);  // Output user input. This is contatenation of variables and strings
   }
 }
