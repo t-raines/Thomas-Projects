9997db42fa9cbbeaf3ba3b7083808a5dd9272657
Thomas Raines <thomasmraines@gmail.com>
2021-10-20 23:57:22 -0400

    add the data analytics

--- Python Covid Data - Hamilton County/Python Covid Data - Hamilton County.py
+++ Python Covid Data - Hamilton County/Python Covid Data - Hamilton County.py
@@ -1,32 +1,35 @@
 ##Make sure to pip install these packages as well
 ##Also make sure to add the file from the link emailed earlier to the working directory.
 
-from datetime import date
-import pandas
+
+import sys; sys.path
+# from datetime import date
+# import pandas
 import os
-import pandas as pd
-import matplotlib.pyplot as plt
+# import pandas as pd
+# import matplotlib.pyplot as plt
 # Print the current working directory
 print("Current working directory: {0}".format(os.getcwd()))
 
+print(os.getcwd())
 
 
-df = pandas.read_csv('covid19_tn_hamilton - hchd_daily.csv', index_col='deaths_new', parse_dates=['date'])
-print(df)
+# df = pandas.read_csv('covid19_tn_hamilton - hchd_daily.csv', index_col='deaths_new', parse_dates=['date'])
+# print(df)
 
 
-df = pd.DataFrame(df, columns=['date', 'cases_new'])
+# df = pd.DataFrame(df, columns=['date', 'cases_new'])
 
-df.plot(y = 'cases_new' , x = 'date', kind = 'scatter')
+# df.plot(y = 'cases_new' , x = 'date', kind = 'scatter')
 
-df.plot(y = 'cases_new' , x = 'date', kind = 'line')
+# df.plot(y = 'cases_new' , x = 'date', kind = 'line')
 
-df.plot(y = 'cases_new' , x = 'date', kind = 'bar')
+# df.plot(y = 'cases_new' , x = 'date', kind = 'bar')
 
-plt.xlabel("Dates")
+# plt.xlabel("Dates")
 
-plt.ylabel("Case Counts")
+# plt.ylabel("Case Counts")
 
-plt.title("Covid Cases In Hamilton County By Date")
+# plt.title("Covid Cases In Hamilton County By Date")
 
-plt.show()
+# plt.show()
