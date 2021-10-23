614aed66ac2c36e24845820f3d7d2538c3bab5ad
Thomas Raines <thomasmraines@gmail.com>
2021-10-22 20:17:42 -0400

    blblsbllsbl

--- Python Covid Data - Hamilton County/Python Covid Data - Hamilton County.py
+++ Python Covid Data - Hamilton County/Python Covid Data - Hamilton County.py
@@ -3,16 +3,19 @@
 
 
 import sys; sys.path
-# from datetime import date
+from datetime import date
 import pandas
 import os
 import pandas as pd
-# import matplotlib.pyplot as plt
-# Print the current working directory
+import matplotlib.pyplot as plt
+##Print the current working directory
 print("Current working directory: {0}".format(os.getcwd()))
 
 print(os.getcwd())
 
+os.chdir("D:\CodingProjects\Thomas' Projects\Thomas-Projects\Python Covid Data - Hamilton County")
+
+print(os.getcwd())
 
 df = pandas.read_csv('covid19_tn_hamilton - hchd_daily.csv', index_col='deaths_new', parse_dates=['date'])
 print(df)
